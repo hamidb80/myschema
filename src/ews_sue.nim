8a94638906c536048a5be3d447660e9fe761c6cb
hamidb80 <hr.bolouri@gmail.com>
2022-07-04 20:23:38 +0430

    initial lexer

--- src/ews_sue.nim
+++ src/ews_sue.nim
@@ -1,49 +1,138 @@
-import std/[strutils]
+import std/[strutils, nre, strformat, options, sequtils, macros]
 
 type
-  ParserStates = enum
-    psModule
-    psComment
+  ParserState = enum
+    psModule, psSchematic, psIcon
 
-    psProcName
-    psProcBody
-    psProcArgs
+template err(msg: string): untyped =
+  raise newException(ValueError, msg)
 
-    psCurlyBracket
+func hasLetter(s: string): bool =
+  for ch in s:
+    if ch in Letters:
+      return true
 
-    psIdent
-    psNumber
-    psCommand
+template findGoImpl(s, pat, i): untyped {.dirty.} =
+  let m = s.find(pat, i)
+  assert issome m, "cannot match"
 
+  i = m.get.matchBounds.b
 
-proc lex(code: string) =
-  var pStateStack = @[psModule]
-  
-  template enter(s): untyped =
-    pStateStack.add s
+func findGo(s: string, pat: Regex, i: var int): string =
+  findGoImpl s, pat, i
+  m.get.captures[0]
 
-  template exit(): untyped =
-    pStateStack.del pStateStack.high
-    
+func findGoMulti(s: string, pat: Regex, i: var int): seq[string] =
+  findGoImpl s, pat, i
+  for s in m.get.captures:
+    result.add s.get
 
-  for ch in code:
-    case pStateStack[^1]:
-    of psModule:
-      case ch:
-      of '#': enter psComment
-      of '\n': exit
-      else: discard
+macro toTuple(list: untyped, n: static[int]): untyped =
+  let tempId = ident "temp"
+  var tupleDef = newTree nnkTupleConstr
 
-    of psComment: discard
-    of psProcName: discard
-    of psProcBody: discard
-    of psProcArgs: discard
-    of psCurlyBracket: discard
-    of psIdent: discard
-    of psNumber: discard
-    of psCommand: discard
+  for i in 0..(n-1):
+    tupleDef.add newTree(nnkBracketExpr, tempid, newlit i)
 
+  quote:
+    block:
+      let `tempId` = `list`
+      `tupleDef`
+
+# loc: line of code
+proc matchProcLine(loc: string) =
+  var i = 0
+  let command = loc.findGo(re"(\w+) ", i)
+
+  # --- parse params
+
+  case command:
+  of "make":
+    let moduleName = loc.findGo(re"(\w+) ", i)
+
+  of "make_wire":
+    let (x1, y1, x2, y2) = loc
+      .findGoMulti(re"(-?\d+) (-?\d+) (-?\d+) (-?\d+)", i)
+      .mapit(parseInt it)
+      .toTuple(4)
+
+  of "make_text":
+    discard
+
+  # of "icon_setup":
+  #   discard
+
+  # of "icon_term":
+  #   discard
+
+  # of "icon_property":
+  #   discard
+
+  of "icon_line":
+    let args = loc.substr(i+1).split.mapit(parseInt it)
+    echo args
+    i = loc.high
+
+  # of "icon_arc":
+  #   discard
+
+  else:
+    discard
+    # err fmt"undefined command: {command}"
+
+  # parse options
+
+  while i < loc.high:
+    let flag =
+      try: loc.findgo(re"-(\w+) ", i)
+      except: break
+
+    # echo flag
+
+    case flag:
+    of "origin":
+      let (x, y) = loc
+        .findGoMulti(re"\{(-?\d+) (-?\d+)\}", i)
+        .mapit(parseInt it)
+        .toTuple(2)
+
+      echo (x, y)
+
+    of "orient":
+      discard
+
+    of "name", "type", "size", "anchor", "label", "rotate":
+      echo loc.findGo(re"(\w+)", i)
+
+    of "text":
+      echo loc.findGo(re"(\{.*\}|[^ ]+)", i)
+
+    else:
+      err fmt"invalid flag: {flag}"
 
 when isMainModule:
-  let content = readfile "./examples/eg1.sue"
-  lex content
+  var pstate = psModule
+
+  for loc in lines "./examples/eg1.sue":
+
+    if (not hasLetter loc) or (loc.startsWith '#'): # empty line or comment
+      discard
+
+    elif loc.startsWith '}': # end of proc body
+      pstate = psModule
+
+    elif loc.startsWith "proc": # proc def
+      let
+        res = loc.find(re"([a-zA-Z]+)_", "proc ".len).get
+        prefix = res.captures[0]
+
+      pstate = case prefix:
+        of "SCHEMATIC": psSchematic
+        of "ICON": psIcon
+        else: err "invalid proc name"
+
+    elif pstate != psModule:
+      matchProcLine loc.substr 2
+
+    else:
+      err fmt"cannot happen: {loc}"
