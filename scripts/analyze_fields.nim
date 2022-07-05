bb291b5032d46c07bd224ae0ae005c4c240830fa
hamidb80 <hr.bolouri@gmail.com>
2022-07-05 19:26:29 +0430

    improve sciprt

--- scripts/analyze_fields.nim
+++ scripts/analyze_fields.nim
@@ -3,29 +3,31 @@ import ../src/sue/parser
 import print
 
 
-var res: array[SueFlags, HashSet[string]]
-var customs: Table[string, HashSet[string]]
+var 
+  uniqValuesForFields: array[SueFlags, HashSet[string]]
+  customs: Table[string, HashSet[string]]
+  fieldsForCommands: array[SueCommands, HashSet[SueFlags]]
 
-iterator expressions(sf: SueFile): SueExpression = 
+iterator expressions(sf: SueFile): SueExpression =
   for expr in sf.icon:
     yield expr
 
   for expr in sf.schematic:
     yield expr
 
-const 
+const
   me = r"C:\Users\HamidB80\Desktop\set4\2-cpu\file\sue\"
   full = r"C:\Users\HamidB80\Desktop\set4"
 
 for path in walkDirRec full:
   if path.endsWith ".sue":
     # echo "\n\n>> ", path, "\n\n"
-
     let s = parseSue readfile path
 
     for expr in expressions s:
       for o in expr.options:
         let f = o.flag
+
         if f == sfCustom:
           if o.field notin customs:
             customs[o.field] = initHashSet[string]()
@@ -33,8 +35,15 @@ for path in walkDirRec full:
           customs[o.field].incl o.value
 
         elif f notin {sfText, sfName, sfLabel, sfOrigin}:
-          res[f].incl dumpValue o
+          uniqValuesForFields[f].incl dumpValue o
+
+        fieldsForCommands[expr.command].incl f
 
 
-print res
+# ----------------------------------
+
+print uniqValuesForFields
 print customs
+
+for c, fs in fieldsForCommands:
+  echo c, ": ", fs
