bae5e6847a1c4e098a3128caa53126d2faf40d02
hamidb80 <hr.bolouri@gmail.com>
2022-07-13 14:17:16 +0430

    svg module

--- src/common/defs.nim
+++ src/common/defs.nim
@@ -1,3 +1,6 @@
 type
   Point* = tuple[x,y: int]
+  
   Range*[T] = HSlice[T, T]
+  
+  Percent* = range[0.0 .. 1.0]
--- src/middle/svg.nim
+++ src/middle/svg.nim
@@ -1,9 +1,34 @@
 import std/[xmltree]
 import ../common/defs
 
-func newCanvas*(x, y, w, h: int): XmlNode = discard
-func newRect*(x, y, w, h: int): XmlNode = discard
-func newCircle*(cx, cy, r: int): XmlNode = discard
-func newLine*(points: seq[Point]): XmlNode = discard
-func newText*(font, size, weight: string): XmlNode = discard
-func newGroup*(children: seq[XmlNode]): XmlNode = discard
+type
+  Color* = object
+    r, g, b: range[0..255]
+    a: Percent
+
+  DesignSettings* = object
+    stroke: Color
+    strokeWidth: float
+    fill: Color
+
+
+func newCanvas*(x, y, w, h: int): XmlNode =
+  discard
+
+func newGroup*(children: seq[XmlNode]): XmlNode =
+  discard
+
+func newRect*(x, y, w, h: int): XmlNode =
+  discard
+
+func newCircle*(cx, cy, r: int): XmlNode =
+  discard
+
+func newLine*(points: seq[Point]): XmlNode =
+  discard
+
+func newPoly*(points: seq[Point]): XmlNode =
+  discard
+
+func newText*(font, size, weight: string): XmlNode =
+  discard
