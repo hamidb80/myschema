35cbdec3c5f8f82de816f13ff1fd0e15da4e1685
hamidb80 <hr.bolouri@gmail.com>
2022-07-14 19:00:18 +0430

    add ids

--- src/ease/parser.nim
+++ src/ease/parser.nim
@@ -1,31 +1,45 @@
 import std/[tables]
+import lisp
 
 type
   Properties = Table[string, string]
+  # Attributes =
 
   HDL_Ident* = object
     name: string
-    # username: int
-    # atrributes: 
+    username: int
+    # atrributes:
 
-  PackageInfo* = ref object
+type
+  Package* = ref object
     obid, library, name: string
-
-  Design* = object # AKA library
-    name, obid: string
 
   WorkSpace* = ref object
     obid: string
     properties: Properties
-    designs: seq[Design]
-    # packages: seq[PackageInfo]
-    # usedPackages: seq[tuple[suffix: string, pkg: PackageInfo]]
+    designs: seq[Library]
+    # packages: seq[Package]
+    # usedPackages: seq[tuple[suffix: string, pkg: Package]]
 
   Library* = ref object
     obid: string
+    name: string
     properties: Properties
 
   Entity* = ref object
+
+type
+  LibraryEncodeMode* = enum
+    lemRef
+    lemDef
+
+  EntityEncodeMode* = enum
+    eemRef
+    eemDef
+
+
+func toLispNode(l: Library, mode: LibraryEncodeMode): LispNode = discard
+func toLispNode(l: Entity, mode: LibraryEncodeMode): LispNode = discard
 
 
 
@@ -35,7 +49,7 @@ type
 ...
 (END_OF_FILE)
 ]#
-  
+
 # --- project.eas
 #[
 (PROJECT_FILE
@@ -47,7 +61,7 @@ type
 
   (PACKAGE "pkg_name" "id") ...
   (PACKAGE_USE ...) ...
-) 
+)
 ]#
 
 # --- lib<id>/library.eas
@@ -62,7 +76,7 @@ type
   (ENTITY "entity_name" "id") ...
   (PACKAGE "pkg_name" "id") ...
   (PACKAGE_USE ...) ...
-) 
+)
 ]#
 
 
@@ -94,7 +108,7 @@ type
     (TYPE <TYPE_NO>)
     (SCHEMATIC ...)
   ) ...
-) 
+)
 ]#
 
 # =================================
@@ -105,12 +119,42 @@ type
   (OBID)
   (SHEETSIZE 0 0 <Width> <Height>)
 
+  (FREE_PLACED_TEXT)
   (GENERIC) ...
+  (COMPONENT) ...
   (PROCESS) ...
   (PORT) ...
   (NET) ...
 )
 
+(FREE_PLACED_TEXT
+  (LABEL)
+)
+
+(COMPONENT
+  (OBID "Comp<id>")
+  (HDL_IDENT)
+  (GEOMETRY)
+  (SIDE)
+  (LABEL)
+  (ENTITY "<lib_id>" "<entity_id>")
+)
+
+(LABEL
+  (POSITION)
+  (SCALE)
+  (COLOR_LINE)
+  (SIDE)
+  (ALIGNMENT)
+  (FORMAT 1)
+  (TEXT "Instruction Decoder")
+)
+
+(ALIGNMENT 0..8)
+(SIDE 0..3)
+(COLOR_LINE N)
+(SCALE N)
+(POSITION X Y)
 
 (HDL_IDENT
   (NAME "halt")
@@ -124,7 +168,7 @@ type
   (MODIFIED 1340886716 "Thu Jun 28 17:01:56 2012")
 )
 
-(LABEL 
+(LABEL
   (POSITION 1384 128)
   (SCALE 96)
   (COLOR_LINE 0)
@@ -144,35 +188,87 @@ type
 
   # ARCH_DEF:
     (CONNECTION)
-)
-
-(NET
-  (OBID)
-  (HDL_IDENT)
-  
-  (PART
-    (OBID "nprt<UNIQ_ID>")
-    (CBN 1)
   )
-  (PART
-    (OBID "nprt<UNIQ_ID>")
+
+  (NET
+    (OBID)
+    (HDL_IDENT)
+
+    (PART
+      (OBID "nprt<UNIQ_ID>")
+      (CBN 1)
+    )
+    (PART
+      (OBID "nprt<UNIQ_ID>")
+      (LABEL)
+
+      (WIRE) ...
+
+      (PORT
+        (OBID "<Port_Instance_Id>")
+        (NAME "<Port_Name>")
+      ) ... (2+)
+    )
+  )
+
+  (CONNECTION
+    (OBID)
+    (GEOMETRY X1 Y1 X1 Y1)
+    (SIDE 0)
     (LABEL)
-  
-    (WIRE) ...
-    
-    (PORT 
-      (OBID "<Port_Instance_Id>")
-      (NAME "<Port_Name>")
-    ) ... (2+)
   )
-)
 
-(CONNECTION 
-  (OBID)
-  (GEOMETRY X1 Y1 X1 Y1)
-  (SIDE 0)
-  (LABEL)
-)
+  (WIRE X1 Y1 X2 Y2)
+  ]#
 
-(WIRE X1 Y1 X2 Y2)
+
+# -----------------------------------
+#[
+
+IDS:
+  proj => project
+  ent => entity
+  lib => library
+
+  eprt => entity port
+  cprt => connection port
+  aprt => architecture port
+  nprt => net port
+  pprt => process port
+
+  proc => process
+
+  thdr => t header
+  trow => t row
+  ttab => t table
+  cell => cell
+
+  ncon => node connection
+
+  diag => diagram
+
+  Comp => component
+
+  pack => package
+
+  igen => ??? generic
+  egen => ???
+
+  Arch => ???
+  file => ???
+  fsm => ???
+  lab => ???
+  itxt => ???
+
+  tran => ???
+  stat => state
+
+  act => action
+
+  net => net
+
+  hook => bus ripper
+
+  cbn => ?
+
 ]#
