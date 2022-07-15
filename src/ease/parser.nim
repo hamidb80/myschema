4390b8ce7882f0d9b5f70173de83ef9ffe143ff7
hamidb80 <hr.bolouri@gmail.com>
2022-07-15 07:44:16 +0430

    extract eas info

--- src/ease/parser.nim
+++ src/ease/parser.nim
@@ -10,6 +10,57 @@ type
     username: int
     # atrributes:
 
+# Ease HDL 8.0
+
+type
+  ProcessTypes* = enum
+    ptProcess = 1
+    ptStateDiagram = 2
+    ptConcurrentStatement = 3
+    ptTruthTable = 5
+
+  GenerateBlockType* = enum
+    gbtForGenerate = 1
+    gbtIfGenerate = 2
+
+  EntityType* = enum
+    etBlockDiagram = 1
+    etHDLFile
+    etStateDiagram
+    etTableDiagram
+    etExternalHDLFIle
+
+  PortMode* = enum
+    pmInput = 1
+    pmOutput
+    pmInout
+    pmBuffer
+
+
+  Alignment* = enum
+    BottomRight = 0
+    Bottom = 1
+    BottomLeft = 2
+    Right = 3
+    Center = 4
+    Left = 5
+    TopRight = 6
+    Top = 7
+    TopLeft = 8
+    # 8 7 6
+    # 5 4 3
+    # 2 1 0
+
+  Side* = enum
+    TopToBottom
+    RightToLeft
+    BottomToTop
+    LeftToRight
+    #   0
+    # 3   1
+    #   2
+
+
 type
   Package* = ref object
     obid, library, name: string
@@ -28,6 +79,8 @@ type
 
   Entity* = ref object
 
+
+
 type
   LibraryEncodeMode* = enum
     lemRef
@@ -39,7 +92,6 @@ type
 
 
 func toLispNode(l: Library, mode: LibraryEncodeMode): LispNode = discard
-func toLispNode(l: Entity, mode: LibraryEncodeMode): LispNode = discard
 
 
 
@@ -119,8 +171,9 @@ func toLispNode(l: Entity, mode: LibraryEncodeMode): LispNode = discard
   (OBID)
   (SHEETSIZE 0 0 <Width> <Height>)
 
-  (FREE_PLACED_TEXT)
+  (FREE_PLACED_TEXT) ...
   (GENERIC) ...
+  (GENERATE) ...
   (COMPONENT) ...
   (PROCESS) ...
   (PORT) ...
@@ -186,41 +239,73 @@ func toLispNode(l: Entity, mode: LibraryEncodeMode): LispNode = discard
   (SIDE 3)
   (LABEL ...)
 
+  if GENERATE:
+    (GENERATE "port ref ")
+
   # ARCH_DEF:
     (CONNECTION)
+)
+
+(NET
+  (OBID)
+  (HDL_IDENT)
+
+  (PART
+    (OBID "nprt<UNIQ_ID>")
+    (CBN 1)
   )
-
-  (NET
-    (OBID)
-    (HDL_IDENT)
-
-    (PART
-      (OBID "nprt<UNIQ_ID>")
-      (CBN 1)
-    )
-    (PART
-      (OBID "nprt<UNIQ_ID>")
-      (LABEL)
-
-      (WIRE) ...
-
-      (PORT
-        (OBID "<Port_Instance_Id>")
-        (NAME "<Port_Name>")
-      ) ... (2+)
-    )
-  )
-
-  (CONNECTION
-    (OBID)
-    (GEOMETRY X1 Y1 X1 Y1)
-    (SIDE 0)
+  (PART
+    (OBID "nprt<UNIQ_ID>")
     (LABEL)
+
+    (WIRE) ...
+
+    (PORT
+      (OBID "<Port_Instance_Id>")
+      (NAME "<Port_Name>")
+    ) ... (2+)
   )
+)
 
-  (WIRE X1 Y1 X2 Y2)
-  ]#
+(CONNECTION
+  (OBID)
+  (GEOMETRY X1 Y1 X1 Y1)
+  (SIDE 0)
+  (LABEL)
+)
 
+(WIRE X1 Y1 X2 Y2)
+
+(GENERATE
+  (OBID)
+  (PROPERTIES
+    (PROPERTY "IF_CONDITION" "my_cond")
+    (PROPERTY "FOR_LOOP_VAR" "ident")
+  )
+  (HDL_IDENT)
+  (GEOMETRY)
+  (SIDE)
+  (LABEL)
+
+  (TYPE 2)
+
+  if for:
+    (CONSTRAINT
+      (DIRECTION 1)
+      (RANGE "max" "min")
+    )
+
+  (SCHEMATIC
+    (OBID)
+    (SHEETSIZE)
+  )
+)
+
+(EXTERNAL_FILE
+  (OBID "extff700001022bc0d264002b4d2a9a05a77")
+  (HDL_IDENT)
+  (FILE <Path>)
+)
 
 (TABLE
   (OBID)
@@ -244,53 +329,59 @@ func toLispNode(l: Entity, mode: LibraryEncodeMode): LispNode = discard
   (LABEL)
 )
 
+(HDL_FILE
+  (VHDL_FILE
+    (OBID)
+    (NAME "pr0.vhd")
+    (VALUE "lines of the file" ...)
+  )
+)
 
 # -----------------------------------
-#[
+
 
 OBID:
   proj => project (PROJECT_FILE)
   pack => package (PROJECT_FILE->PACKAGE | PACKAGE_FILE)
   lib => library (DESIGN_FILE)
   ent => entity (ENTITY_FILE->ENTITY)
-  Arch => architecture (ARCH_DEFINITION)
+  Arch/arch => architecture (ARCH_DEFINITION)
 
   eprt => entity port (ENTITY->PORT)
   cprt => component port (COMPONENT->PORT)
   aprt => architecture port (ARCH_DEFINITION->PORT)
   nprt => net port (NET->PART->PORT)
   pprt => process port (PROCESS->PORT)
+  gprt => generate port (GENERATE->PORT)
 
   proc => process (PROCESS)
+  genb => generate block
 
   ttab => truth table (TABLE)
-  thdr => truth header (HEADER)
-  trow => truth row (ROW)
+  thdr => table header (HEADER)
+  trow => table row (ROW)
   cell => cell (CELL)
 
+  Comp/comp => component [instance of a block] (COMPONENT)
   ncon => node connection (CONNECTION)
-  
-  Comp => component (COMPONENT)
+  net => net (NET)
+
+  hook => bus ripper (BUS_RIPPER)
+  cbn => ??? (CBN)
 
   igen => instance generic (GENERIC)
   egen => entity generic (GENERIC)
 
   file => file content (VHDL_FILE)
-  lab => ??? (ACTION | CONDITION)
   itxt => included text (FSM_DIAGRAM->INCLUDED_TEXT)
+  extf => external file
 
   fsm => (STATE_MACHINE_V2 | TRANS_LINE/TRANS_SPLINE->[FROM_CONN, TO_CONN] | GLOBAL)
   diag => diagram (SCHEMATIC | FSM_DIAGRAM)
   tran => transition (TRANS_LINE | TRANS_SPLINE)
+  lab => ??? (ACTION | CONDITION)
   stat => state (STATE)
   lab => (ACTION | CONDITION)
-
   act => action (ACTION)
-
-  net => net (NET)
-
-  hook => bus ripper (BUS_RIPPER)
-
-  cbn => ??? (CBN)
 
 ]#
