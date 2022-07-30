2783a3c4be65a33d2ee4850c8465ed3c4eea103a
hamidb80 <hr.bolouri@gmail.com>
2022-07-30 14:41:32 +0430

    add tests for rotation

--- tests/tcommon/tcoordination.nim
+++ tests/tcommon/tcoordination.nim
@@ -4,11 +4,23 @@ import src/common/coordination
 suite "rotation":
   test "rotate0":
     let p = (-4, -3)
-    
+
     check rotate0(p, r0) == p
     check rotate0(p, r90) == (3, -4)
     check rotate0(p, r180) == (4, 3)
     check rotate0(p, r270) == (-3, 4)
 
   test "rotate":
-    discard
+    let
+      c1 = (10, 40)
+      p1 = (10, 45)
+    check p1.rotate(c1, -r90) == (15, 40)
+    check p1.rotate(c1, r90) == (5, 40)
+
+    let
+      c2 = (8, -7)
+      p2 = (5, 1)
+
+    check p2.rotate(c2, r180) == (11, -15)
+    check p2.rotate(c2, -r90) == (16, -4)
+    check p2.rotate(c2, r90) == (0, -10)
